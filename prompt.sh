91bf5377db72936823556b1c4f4ca7f5262cdef9
Aaron Gray <aarogray@amazon.com>
2013-11-20 22:40:39 -0800

    Differentiate between tracked/untracked changes
    Also inserted trailing spaces to play more nicely with my prompt

--- prompt.sh
+++ prompt.sh
@@ -5,18 +5,26 @@ find_git_branch() {
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
-    git_branch="($branch)"
+    git_branch="($branch) "
   else
     git_branch=""
   fi
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
+  # are there any changes in tracked files?
+  local status_tracked=$(git status -uno --porcelain 2> /dev/null)
+  local status_untracked=$(git status --porcelain 2> /dev/null | grep "?? ")
+  if [[ "$status_tracked" != "" ]]; then
+    git_dirty='!'
   else
-    git_dirty=''
+    git_dirty=""
+  fi
+  if [[ "$status_untracked" != "" ]]; then
+    git_dirty="$git_dirty*"
+  fi
+  if [[ "$git_dirty" != "" ]]; then
+    git_dirty="$git_dirty "
   fi
 }
 
@@ -30,3 +38,4 @@ PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
 
 # Default Git enabled root prompt (for use with "sudo -s")
 # export SUDO_PS1="\[$bakred\]\u@\h\[$txtrst\] \w\$ "
+
